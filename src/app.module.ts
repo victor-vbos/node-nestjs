4476891cc0570199cd9921059fd20a1f14a0fd24
victor.silva <victor.silva@gran.com>
2025-03-13 14:56:17 -0300

    feat: Adiciona health check para verificar conexão com banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SalasModule } from './salas/salas.module';
 import { MentoriasModule } from './mentorias/mentorias.module';
+import { HealthController } from './health/health.controller';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [SalasModule, MentoriasModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [SalasModule, MentoriasModule, ConfigModule.forRoot({ isGlobal: true }), HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
